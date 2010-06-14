e18ff925b6a72787e54d65fbc15e6743b8152bf6
Marcos Ojeda <marcos@csail.mit.edu>
2010-06-14 18:21:44 -0400

    new css, still need to fix button, but Pen and Line work

--- web/js/jquery.dbn.js
+++ web/js/jquery.dbn.js
@@ -20,11 +20,22 @@
     // console.debug(code);
     
     for(var i=0; i<code.length; i+=1){
-      var instr = code[i];
-      console.log(instr)
-      if(instr[0].toLowerCase() == "paper"){
-        paper.fillStyle = "rgb("+instr[1]+"%,"+instr[1]+"%,"+instr[1]+"%)";
+      var instr = code[i][0].toLowerCase();
+      var args = code[i].slice(1);
+      
+      if(instr == "paper"){
+        paper.fillStyle = "rgb("+args[0]+"%,"+args[0]+"%,"+args[0]+"%)";
         paper.fillRect(0,0,100,100);
+      }
+      if(instr == "pen"){
+        paper.strokeStyle = "rgb("+args[0]+"%,"+args[0]+"%,"+args[0]+"%)";
+      }
+      if(instr == "line"){
+        paper.beginPath();
+        paper.moveTo(args[0],100-args[1]);
+        paper.lineTo(args[2],100-args[3]);
+        paper.closePath();
+        paper.stroke();
       }
     }
 
