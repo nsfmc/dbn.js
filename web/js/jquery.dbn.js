796c3c6b316884e2ce72775c4c296f3dcd5fbd3d
Marcos Ojeda <marcos@csail.mit.edu>
2010-06-14 14:34:24 -0400

    working webpage with ability to change paper color

--- web/js/jquery.dbn.js
+++ web/js/jquery.dbn.js
@@ -10,14 +10,23 @@
     var paper = ( this.attr("tagName").toLowerCase() === "canvas" ) ? 
       this : $("<canvas />",paperProps).appendTo(this);
     // from now on, paper is the canvas element
+    paper = paper[0].getContext("2d");
     
     code = code.split("\n").
       filter(function(e,i,a){return $.trim(e) !== "";}).
       map($.trim).
       map(function(e,i,a){return e.split(/\s+/);}); // split on all stretches of white space " "
     // code is now an array of split values
-    console.debug(code);
+    // console.debug(code);
     
+    for(var i=0; i<code.length; i+=1){
+      var instr = code[i];
+      console.log(instr)
+      if(instr[0].toLowerCase() == "paper"){
+        paper.fillStyle = "rgb("+instr[1]+"%,"+instr[1]+"%,"+instr[1]+"%)";
+        paper.fillRect(0,0,100,100);
+      }
+    }
 
     this.each(function() {
       // do nothing here yet
